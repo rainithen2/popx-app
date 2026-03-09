1bfb7771a697d803141508160c8039d40f521cf8
Nithen Rai <rainithen2@gmail.com>
2026-03-10 00:57:09 +0530

    fix import paths after folder restructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { BrowserRouter , Routes, Route } from 'react-router-dom'
-import Welcome from './pages/Welcome/Welcome.jsx'   
-import SignIn from './pages/SignIn/SignIn.jsx'      
-import SignUp from './pages/SignUp/SignUp.jsx'      
-import Account from './pages/Account/Account.jsx'
+import Welcome from './component/Welcome'
+import SignIn from './component/SignIn'
+import SignUp from './component/SignUp'
+import Account from './component/Account'
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path='/' element={<Welcome/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
         <Route path='/sign-up' element={<SignUp/>} />
-        <Route path="/account" element={<Account/>} />
+        <Route path='/account' element={ <Account/>}/>
       </Routes>
     </BrowserRouter>
   )
